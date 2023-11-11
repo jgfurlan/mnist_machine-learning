7ea4788c085d04c49a048c431c04c3ffeec68bef
jgfurlan <joaogfurlan@hotmail.co.uk>
2023-11-11 12:31:16 +0000

    Data Container

---
+++ include/data.hpp
@@ -0,0 +1,32 @@
+#ifndef __DATA_H
+#define __DATA_H
+
+#include <cstdint>
+#include <vector>
+#include <stdint.h>
+#include <stdio.h>
+
+/*
+Our Data Container below
+*/
+
+class data {
+    std::vector<uint8_t> * feature_vector; // No class at end
+    uint8_t label;
+    int enum_label; // A -> 1, B -> 2 
+
+    public:
+    void set_feature_vector(std::vector<uint8_t> *);
+    void append_to_feature_vector(uint8_t);
+    void set_label(uint8_t);
+    void set_enumerated_label(int);
+
+    int get_feature_vector_size();
+    uint8_t get_label();
+    uint8_t get_enumerated_label();
+
+    std::vector<uint8_t> * get_feature_vector();
+
+};
+
+#endif
